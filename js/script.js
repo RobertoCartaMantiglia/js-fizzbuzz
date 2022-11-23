e00c16c177eb83b8b57ddb7bd71ae908662d3677
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-23 16:16:18 +0100

    ciclo for buzz e fizz

--- js/script.js
+++ js/script.js
@@ -12,4 +12,11 @@ let i;
 for (let i = 1; i <= 100; i++) 
 {
     console.log(i);
+
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+
 }
